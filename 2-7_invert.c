5336101174c44ccbb8352aead86dcb93c43099d5
Data <95385516+thedataelemental@users.noreply.github.com>
2022-11-17 20:24:07 -0800

    Add files via upload

--- 2-7_invert.c
+++ 2-7_invert.c
@@ -1,29 +1,64 @@
 // 2-7_invert.c
-// Returns x after inverting the n bits of x starting at position p
-// Solved 11/16/2022
-
+// Returns 'input_num' after inverting 'num_of_bits' bits,
+// starting at position 'start_pos'.
+// Exercise from K&R C
+// Solved 11/17/2022
 
 #include <stdio.h>
 
-unsigned invert(unsigned x, int p, int n)
+// Invert designated bits of input number
+unsigned invert(unsigned input_num, int start_pos, int num_of_bits)
 {
-	return (x = (~x & (~(~0 << p) & (~0 << (p - n)))) | (x & (~(~(~0 << p) & (~0 << (p - n))))));
+// Construct bit mask to grab bits we want to invert
+	
+	// This mask grabs all bits that fall right of the left end of the 
+	// inversion zone
+	int inversion_mask_pt_1 = ~(~0 << start_pos);
+	
+	// This mask grabs all bits that fall left of the right end of the 
+	// inversion zone
+	int inversion_mask_pt_2 = (~0 << (start_pos - num_of_bits));
+	
+	// This combination of the above two masks grabs all the bits
+	// within the inversion zone, and nothing else
+	int inversion_mask = (inversion_mask_pt_1 & inversion_mask_pt_2);
+	
+	
+// Construct bit mask to grab bits we want to keep the same
+	
+	// This mask grabs all bits that fall left of the inversion zone
+	int non_inversion_mask_pt_1 = ((~0 << start_pos));
+	
+	// This mask grabs all bits that fall right of the inversion zone
+	int non_inversion_mask_pt_2 = ~(~0 << (start_pos - num_of_bits));
+	
+	// This combination of the above two masks grabs all bits that fall
+	// outside the range of the inversion zone
+	int non_inversion_mask = (non_inversion_mask_pt_1 | non_inversion_mask_pt_2);
+	
+	
+// Apply all of the above masks at once
+	return (~input_num & inversion_mask) | (input_num & non_inversion_mask);
 }
 
+// Get function arguments from user input and call bit flipping function
 int main(void)
 {
-	unsigned x;
-	int p;
-	int n;
+	unsigned input_num; // Input number, here to have some bits inverted
+	int start_pos; // Position at which to begin inverting bits
+	int num_of_bits; // Number of bits to invert (counted to the right)
 	
-	scanf("%u", &x);
-	scanf("%d", &p);
-	scanf("%d", &n);
+	// Get arguments
+	scanf("%u", &input_num);
+	scanf("%d", &start_pos);
+	scanf("%d", &num_of_bits);
 	
-	int y;
+	int output;
 	
-	y = invert(x, p, n);
+	// Work happens here
+	output = invert(input_num, start_pos, num_of_bits);
 	
-	printf("\nOutput: %u", y);
+	// Print final output
+	printf("\nOutput: %u", output);
 	
 }
